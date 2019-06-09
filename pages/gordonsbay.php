23aa91f8afb244cfeaaee272ca1177560b64c04a
bennie <bennie@hi.guru>
2019-06-09 22:55:01 +0200

    generalized main house view to easily swap out info

---
+++ pages/gordonsbay.php
@@ -0,0 +1,41 @@
+<?php
+$title='Gordons Bay';
+$sub_title='Holiday Home';
+$address='171 Bikini Beach';
+$about = <<< 'EOD'
+<p>Well located on Bikini Beach in Gordons Bay, 171 Bikini Beach offers spacious seaside living, with sea and harbour views.</p>
+<p>The house can accommodate up to 10 guests in 4 bedrooms. (From 15 December - 5 bedrooms are
+    available with max 12 guests) Three bedrooms are located inside the house. All three
+    bedrooms have views on Bikini Beach and the old harbour. Bedding and towels are provided.</p>
+<p>The first has a double and single bed with en suite bathroom with a toilet, shower and basin.</p>
+<p>The second bedroom has a double bed.</p>
+<p>The third bedroom has 2 single beds and an en suite bathroom with a toilet, shower and basin
+    inside the room.</p>
+<p>Outside there is a room with two single beds and a seperate bathroom with a shower, toilet
+    and basin</p>
+<p>Inside there is a seperate toilet and family bathroom with a shower, bath and washing machine.</p>
+<p>The lounge features a flat-screen TV, DStv explora, a Hi-Fi and a DVD player. It has one
+    single sleeper couch. Next to the Lounge is a big undercover porch which overlooks the
+    beach. In the kitchen there is a dishwasher, microwave and a four plate stove with oven.</p>
+<p>At the back guests will enjoy the private garden and braai area. A double garage and additional
+    parking in front of the garage is available.
+</p>
+<p>No smoking or animals.</p>
+<p>Furnished Property </p>
+<p>Reference #: 2321283</p>
+EOD;
+$rooms = array(	"Master Bedroom"=>["ms", "images/MS1.jpg", "images/MS2.jpg", "images/MS3.jpg"],
+								"Middle Bedroom"=>["mb", "images/MB1.jpg"],
+								"Guest Bedroom"=>["gb", "images/GB1.jpg", "images/GB2.jpg", "images/GB3.jpg"],
+								"Outside Bedroom"=>["ob", "images/OB1.jpg", "images/OB2.jpg", "images/OB3.jpg", "images/OB4.jpg"],
+								"Lounge"=>["lr", "images/LR1.jpg", "images/LR2.jpg", "images/LR3.jpg", "images/LR4.jpg"],
+								"Dining Area"=>["dr", "images/DR1.jpg", "images/DR2.jpg", "images/DR3.jpg"],
+								"Porch"=>["s", "images/S1.jpg", "images/S2.jpg", "images/S3.jpg"],
+								"Braai Area"=>["b", "images/B1.jpg", "images/B2.jpg", "images/B3.jpg"]);
+$details = <<< 'EOD'
+<li>Bedrooms: 4</li>
+<li>Guests: 10</li>
+<li>Areas: Kitchen, Lounge, Stoop, Garden</li>
+<li>Extras: Washing machine, Dishwasher, Microwave, Stove with oven, Braai, Double Garage</li>
+<li>PLEASE NOTE: No smoking or animals</li>
+EOD;
